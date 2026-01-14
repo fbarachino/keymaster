5c6762b0522b38b48dac0633543fb63f67dcf693
fbarachino <flavio.barachino@gmail.com>
2026-01-14 15:09:31 +0000

    impostato HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+	URL::forceScheme('https');
         $this->app['events']->listen(BuildingMenu::class, function (BuildingMenu $event) {
 
             // Se l'utente non è autenticato, esci subito
